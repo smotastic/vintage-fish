c108744195e52ff73aafb8cbba0028d203589305
Allquantor <nilssmol@gmail.com>
2020-07-15 21:54:22 +0200

    wip components

---
+++ src/pages/CreateTask.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-page padding>
+    Erzeugen
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'CreateTask'
+}
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -7,7 +7,13 @@ const routes = [
       { path: '', component: () => import('pages/Index.vue') }
     ]
   },
-
+  {
+    path: '/createTask',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/CreateTask.vue') }
+    ]
+  },
   // Always leave this as last one,
   // but you can also remove it
   {
