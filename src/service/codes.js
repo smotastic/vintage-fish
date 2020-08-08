c7977bfb39f4aa6b1f25a0e421faef03fcbb09f3
Allquantor <nilssmol@gmail.com>
2020-08-08 23:20:07 +0200

    Task starten / stoppen

--- src/service/codes.js
+++ src/service/codes.js
@@ -1,18 +1,51 @@
 export const errors = {
-    // 0..1000 = CRUD Errors
     TASK_CREATE_ALREADYEXISTS: (task) => ({
         msg: `The task with the summary '${task.summary}' already exists`,
         object: task,
         code: 0
+    }),
+    TASK_START_ALREADYRUNNING: (task) => ({
+        msg: `The task '${task.summary}' is already running`,
+        object: task,
+        code: 100
+    }),
+    TASK_STOP_ALREADYSTOPPED: (task) => ({
+        msg: `The task '${task.summary}' is already stopped`,
+        object: task,
+        code: 101
+    }),
+    TASK_START_ANOTHERTASKISRUNNING: (task, runningTask) => ({
+        msg: `The task '${task.summary}' cannot be started, as task '${runningTask.summary}' is already running`,
+        object: task,
+        code: 102
+    }),
+    UNKNOWN_ERROR: (e) => ({
+        msg: `An unkown error ${e} occured`,
+        code: 999
     })
+
 }
 
 export const success = {
-    // 0..1000 = CRUD Successes
     TASK_CREATE: (task) => ({
         msg: `The task '${task.summary}' was successfully created`,
         object: task,
-        code: 0
+        code: 1000
+    }),
+    TASK_UPDATE: (task) => ({
+        msg: `The task '${task.summary}' was successfully updated`,
+        object: task,
+        code: 1001
+    }),
+    TASK_STARTED: (task) => ({
+        msg: `The task '${task.summary}' was successfully started`,
+        object: task,
+        code: 1002
+    }),
+    TASK_STOPPED: (task) => ({
+        msg: `The task '${task.summary}' was successfully stopped`,
+        object: task,
+        code: 1003
     })
 }
 
--- src/service/index.js
+++ src/service/index.js
@@ -1,3 +1,4 @@
-import { create, readToday } from "./local/local.crud"
+import { create, update, readToday } from "./local/local.crud"
+import { startTask, stopTask } from "./local/local.task";
 
-export default { create, readToday }
+export default { create, update, readToday, startTask, stopTask }
