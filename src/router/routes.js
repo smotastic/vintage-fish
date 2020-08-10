282fbf128a5c89c33511f6d9ed2765c86e82cdb3
Nils Smolinski <nils.smolinski@lyth.de>
2020-08-10 12:05:31 +0200

    Fix typo in Tasklist

--- src/router/routes.js
+++ src/router/routes.js
@@ -5,14 +5,14 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('../pages/TaskList.vue') }
+      { path: '', component: () => import('../pages/Tasklist.vue') }
     ]
   },
   {
     path: Paths.LIST_TASKS,
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('../pages/TaskList.vue') }
+      { path: '', component: () => import('../pages/Tasklist.vue') }
     ]
   },
   // Always leave this as last one,
