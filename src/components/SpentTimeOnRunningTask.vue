bb72512986d1d87f249f51c789615ee0445b4bc4
Allquantor <nilssmol@gmail.com>
2020-08-12 21:42:59 +0200

    Fix Time display bug + See Running Task on Startup

--- src/components/SpentTimeOnRunningTask.vue
+++ src/components/SpentTimeOnRunningTask.vue
@@ -4,8 +4,16 @@
 
 <script>
 import { now } from "../service/util";
+import Service from "../service/";
 export default {
   created() {
+    Service.readToday().then((todaysTasks) => {
+      const runningTask = todaysTasks.find((t) => t.running);
+      if (runningTask) {
+        this.$store.commit("changeTask", runningTask);
+      }
+    });
+
     setInterval(() => {
       if (this.$store.getters.showTime) {
         const start = Date.parse(
@@ -13,10 +21,13 @@ export default {
         );
         const noaw = Date.parse(`1970-01.01 ${now()}`);
         const totalSeconds = (noaw - start) / 1000;
-        const seconds = String(parseInt(totalSeconds % 60)).padStart(2, "0");
-        const minutes = String(parseInt(totalSeconds / 60)).padStart(2, "0");
-        const hours = String(parseInt(minutes / 60)).padStart(2, "0");
-        this.spentTimeOnRunningTask = `${hours}:${minutes}:${seconds}`;
+        const seconds = totalSeconds % 60;
+        const minutes = totalSeconds / 60;
+        const hours = minutes / 60;
+        const hourStr = String(parseInt(hours)).padStart(2, "0");
+        const minStr = String(parseInt(minutes % 60)).padStart(2, "0");
+        const secStr = String(parseInt(seconds % 60)).padStart(2, "0");
+        this.spentTimeOnRunningTask = `${hourStr}:${minStr}:${secStr}`;
       } else {
         this.spentTimeOnRunningTask = "";
       }
